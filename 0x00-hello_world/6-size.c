c8096e47b7f7ae6d0c702ae6791f33a0bf5e6f51
vierukundo <vierukundo@gmail.com>
2023-02-16 12:16:02 -0800

    C programming projects

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program that prints the size of data types
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char: %d bytes \n",  sizeof(char));
+printf("Size of a int: %d bytes \n",  sizeof(int));
+printf("Size of a long int: %d bytes \n",  sizeof(long int));
+printf("Size of a long long int: %d bytes \n",  sizeof(long long int));
+printf("Size of a float: %d bytes \n", sizeof(float));
+return (0);
+}
