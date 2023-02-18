e40cff51453705afcea7255c81faf13f2679f6df
vierukundo <vierukundo@gmail.com>
2023-02-17 23:45:11 -0800

    C programming projects

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,30 @@
 */
 int main(void)
 {
-	int a;
+	int a, b;
 
-	int b;
-
-	int c;
-
-	int d;
-
-	for (a = 0; a < 10; a++)
+	a = 0;
+	while (a <= 98)
 	{
-		for (b = 0; b < 10; b++)
+		b = a + 1;
+		while (b <= 99)
 		{
-			for (c = b + 1; c < 10; c++)
+			if (a < b)
 			{
-				for (d = 0; d < 10; d++)
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
+				putchar(' ');
+				putchar(b / 10 + '0');
+				putchar(b % 10 + '0');
+				if (a != 98 || b != 99)
 				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(' ');
-					putchar(d + '0');
-					putchar(c + '0');
 					putchar(',');
 					putchar(' ');
 				}
 			}
+			b++;
 		}
+		a++;
 	}
 	putchar('\n');
 	return (0);
