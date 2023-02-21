f8bab869d36d76fc74d770cefe0797bccec134d9
vierukundo <vierukundo@gmail.com>
2023-02-21 04:03:06 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
