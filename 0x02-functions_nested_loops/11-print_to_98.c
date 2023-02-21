7e1340efbca025f2c17ddca60fedcdb47cd4b906
vierukundo <vierukundo@gmail.com>
2023-02-21 15:08:48 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,26 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, a;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		printf("%d", i);
-		if (i < 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
-		else if (i == 98)
+	}
+	else if (n >= 98)
+	{
+		for (a = n; a >= 98; a--)
 		{
-			_putchar('\n');
+			if (n == 98 && a == 98)
+				printf("%d\n", a);
+			else
+				printf("%d, ", a);
 		}
 	}
 }
