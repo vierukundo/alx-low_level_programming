033acccbf652ae7974718e3018c2b149f75a5b0c
vierukundo <vierukundo@gmail.com>
2023-02-22 12:58:36 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long int i, n, m, next;
+
+	i = 1;
+	n = 2;
+	next = i + n;
+	printf("%lu, ", i);
+	printf("%lu, ", n);
+	for (m = 3; m <= 98; m++)
+	{
+		if (m == 98)
+			printf("%lu\n", next);
+		else
+			printf("%lu, ", next);
+		i = n;
+		n = next;
+		next = i + n;
+	}
+	return (0);
+}
