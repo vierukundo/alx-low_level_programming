07a7affcd198cdd35da4e7740c4a6d12a9f3c8e9
vierukundo <vierukundo@gmail.com>
2023-02-22 07:02:22 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * findSum - prints the sum of all the
+ * main - prints the sum of all the
  * multiples of 3 or 5 below 1024
- * @a: multiple one
- * @n: limit
- * @b: multiple two
- * Return: sum
+ * Return: 0 (Success)
  */
-int findSum(int n, int a, int b)
+int main(void)
 {
 	int sum = 0;
 
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % a == 0 || i % b == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
 	}
-	return (sum);
+	printf("%d\n", sum);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-int findSum(int n, int a, int b);
