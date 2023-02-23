85c2107183c405b95adbd0a8e6760c1aee27c530
vierukundo <vierukundo@gmail.com>
2023-02-23 05:19:04 -0800

    C - Functions, nested loops

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,21 +16,20 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-			day++;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day == 59)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
+		if (month >= 2 && day >= 59)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+		}
+		else
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 	}
 }
