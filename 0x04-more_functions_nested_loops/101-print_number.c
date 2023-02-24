ca7ba69ff8ddeced12e867918d629df9ff76958a
vierukundo <vierukundo@gmail.com>
2023-02-24 06:21:45 -0800

    C - Functions, nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer
+ * @n: integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		n *= -1;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
