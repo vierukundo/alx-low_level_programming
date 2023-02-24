0a3f5e32b4c939e87149b9a59a05f6ef4e937901
vierukundo <vierukundo@gmail.com>
2023-02-24 07:32:46 -0800

    C - Functions, nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,21 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n *= -1;
+	} else
+	{
+		n1 = n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
