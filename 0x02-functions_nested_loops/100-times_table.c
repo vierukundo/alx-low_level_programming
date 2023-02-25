6c3f1f435ac091ba80374318f57b68b9a7a5430b
vierukundo <vierukundo@gmail.com>
2023-02-25 06:00:53 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,62 +1,46 @@
 #include "main.h"
 /**
- * print_times_table - function that prints the n times table, starting with 0
- * @n: number to be checked
- * Return: 0 (Success)
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int i, j, k;
 
-	for (a = 0; a <= n; a++)
+	if (n >= 0 && n <= 15)
 	{
-		if (n < 0 || n > 15)
-			break;
-		for (b = 0; b <= n; b++)
+		for (i = 0; i <= n; i++)
 		{
-			c = a * b;
-			if (c <= 9 && n >= 0)
+			for (j = 0; j <= n; j++)
 			{
-				if (c == 0 && b <= n)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(c + '0');
-					if (a == 0 && b < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				else if (b <= n && c != 0)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(c + '0');
-				}
-			}
-			else if ((c > 9 && c < 100) && n <= 15)
-			{
-				if (b <= n)
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
 			}
-			else if (c >= 100 && b <= n)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(c / 100 + '0');
-				_putchar((c / 10) % 10 + '0');
-				_putchar(c % 10 + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
---
+++ 0x02-functions_nested_loops/betty_100-times_table.c
@@ -0,0 +1,62 @@
+#include "main.h"
+/**
+ * print_times_table - function that prints the n times table, starting with 0
+ * @n: number to be checked
+ * Return: 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (n < 0 || n > 15)
+			break;
+		for (b = 0; b <= n; b++)
+		{
+			c = a * b;
+			if (c <= 9 && n >= 0)
+			{
+				if (c == 0 && b <= n)
+				{
+					_putchar(c + '0');
+					if (a == 0 && b < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else if (b <= n && c != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+			}
+			else if ((c > 9 && c < 100) && n <= 15)
+			{
+				if (b <= n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (c >= 100 && b <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c / 100 + '0');
+				_putchar((c / 10) % 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
