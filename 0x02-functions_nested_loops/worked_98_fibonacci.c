18a5ea85a98e61ceb5d2d62bf7680f6cdb79b56f
vierukundo <vierukundo@gmail.com>
2023-02-28 13:32:56 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/worked_98_fibonacci.c
+++ 0x02-functions_nested_loops/worked_98_fibonacci.c
@@ -9,23 +9,19 @@ int main(void)
 	unsigned long int i;
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
-	unsigned long int next = 0;
 	unsigned long int l = 1000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
 	unsigned long int aft2;
-	unsigned long int next1 = 0;
-	unsigned long int next2 = 0;
 
 	printf("%lu", bef);
 
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
-		next = bef + aft;
-		bef = aft;
-		aft = next;
+		aft += bef;
+		bef = aft - bef;
 	}
 
 	bef1 = (bef / l);
@@ -37,12 +33,10 @@ int main(void)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
-		next1 = aft1 + bef1;
-		bef1 = aft1;
-		aft1 = next1;
-		next2 = aft2 + bef2;
-		bef2 = aft2;
-		aft2 = next2;
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
