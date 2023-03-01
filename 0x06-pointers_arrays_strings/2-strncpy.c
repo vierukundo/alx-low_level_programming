dd51d54fc2d18e93ba9c4beec91bf8aea93f02db
vierukundo <vierukundo@gmail.com>
2023-03-01 14:19:09 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  * _strncpy - function that copies a string.
  * @src: string one
  * @dest: string two
+ * @n: number of bytes
  * Return: pointer
  */
 char *_strncpy(char *dest, char *src, int n)
