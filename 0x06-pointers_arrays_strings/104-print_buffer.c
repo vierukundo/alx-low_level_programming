e04a0e0baa081daef1911ff0a2016c0ffbf8b45b
vierukundo <vierukundo@gmail.com>
2023-03-04 08:36:58 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,81 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_character - print real characters. if not,
+ * print a period.
+ * @size: size updated
+ * @i: i imported from buffer function
+ * @b: buffer
+ * Return: void
+ */
+void print_character(int size, int i, char *b)
+{
+	int c, m = 0;
 
+	for (c = 0; c < 10; c++)
+	{
+		if (size >= 10)
+		{
+			if (b[(i * 10) + c] >= 32 && b[(i * 10) + c] < 127)
+				printf("%c", b[(i * 10) + c]);
+			else
+				printf("%s", ".");
+		}
+		if (size < 10 && m < size)
+		{
+			if (b[(i * 10) + m] >= 32 && b[(i * 10) + m] < 127)
+			{
+				printf("%c", b[(i * 10) + m]);
+				m++;
+			}
+			else
+			{
+				printf("%s", ".");
+			}
+		}
+		if (m >= size)
+		{
+			break;
+		}
+	}
+}
+/*&
+ * print_bytes - print bytes in hex form
+ * start with number of line in 8 hex characters
+ * @size: size
+ * @i: i from print_buffer function
+ * @n: n imported from buffer function
+ * @b: buffer
+ * Return: void
+ */
+void print_bytes(int size, int i, char *b, int n)
+{
+	int j, s = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (size >= 10)
+		{
+			printf("%02x", b[(i * 10) + j++]);
+			printf("%02x ", b[(i * 10) + n]);
+			n += 2;
+		}
+		else
+		{
+			if (s < size)
+			{
+				printf("%02x", b[(i * 10) + s]);
+				printf("%02x ", b[(i * 10) + n]);
+				n += 2;
+				s += 2;
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	}
+}
 /**
  * print_buffer - prints buffer
  * @b: buffer
@@ -10,41 +85,24 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int i, l, n;
 
-	o = 0;
-
+	if (size % 10 != 0)
+		l = size / 10;
+	else
+		l = (size / 10) - 1;
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	while (o < size)
+	for (i = 0; i <= l; i++)
 	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
+		n = 1;
+		printf("%08x: ", i * 10);
+		print_bytes(size, i, b, n);
+		print_character(size, i, b);
+		size -= 10;
 		printf("\n");
-		o += 10;
 	}
 }
