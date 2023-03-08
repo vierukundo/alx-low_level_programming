2f9fe0f6988ba155f7d341073de8a91954d309d8
vierukundo <vierukundo@gmail.com>
2023-03-08 13:21:13 -0800

    C - Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * find_length - find length of string
+ * @s: string
+ * Return: length
+ */
+int find_length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + find_length(s + 1));
+}
+/**
+ * isPal_or_Not - checks if string is pal
+ * @s: string
+ * @i: iterator
+ * Return: 1 if true and 0 if false
+ */
+int isPal_or_Not(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1 - i) && i < len)
+		return (0);
+	if (i >= len - 1)
+		return (1);
+	return (isPal_or_Not(s, i + 1, len));
+}
+/**
+ * is_palindrome - function that returns 1 if a
+ * string is a palindrome and 0 if not
+ * @s: string
+ * Return: 1 if true and 0 if false
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = find_length(s);
+	return (isPal_or_Not(s, 0, len));
+}
