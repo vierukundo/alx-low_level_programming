3c05974ebdbbae63d93d99bae74eba3cadb97a5a
vierukundo <vierukundo@gmail.com>
2023-03-10 02:21:31 -0800

    C - Static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -5,6 +5,7 @@
  */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be t
+			he best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
