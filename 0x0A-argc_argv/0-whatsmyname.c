ff6929ba62f56b3365c9706f49f3638fab170ab7
vierukundo <vierukundo@gmail.com>
2023-03-10 06:19:03 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - test argv indexed zero
+ * @argc: count
+ * @argv: array of arguments
  * Return: 0
  */
 int main(int __attribute__ ((unused)) argc, char *argv[])
