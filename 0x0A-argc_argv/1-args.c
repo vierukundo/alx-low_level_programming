77cbd99167282c07a0b365eff3ed3b26d93e80cc
vierukundo <vierukundo@gmail.com>
2023-03-10 06:38:53 -0800

    C - argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: arv array
  * Return: 0
  */
-int main (int argc, char __attribute__ ((unused)) *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
