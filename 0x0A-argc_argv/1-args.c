156cc38594ad763dd9bd65489f006beadbbfbbce
vierukundo <vierukundo@gmail.com>
2023-03-10 06:35:47 -0800

    C - argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arg count
+ * @argv: arv array
+ * Return: 0
+ */
+int main (int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
