9b9533a4666daa0b454bac2bc0cbe7505349cf2a
vierukundo <vierukundo@gmail.com>
2023-03-13 08:34:24 -0700

    C - malloc, free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * str_concat - concanates strings
+ * @s1: string one
+ * @s2: string two
+ * Return: concanated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1_size, s2_size;
+
+	char *concanated;
+
+	static char *conc;
+
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+	s1_size = 0;
+	while (s1[s1_size])
+		s1_size++;
+	s2_size = 0;
+	while (s2[s2_size])
+		s2_size++;
+	conc = (char *)malloc(sizeof(char) * (s2_size + 2 + s1_size));
+	if (conc == NULL)
+		return (NULL);
+	concanated = conc;
+	while (*s1)
+	{
+		*concanated = *s1;
+		concanated++;
+		s1++;
+	}
+	*concanated = 32;
+	concanated++;
+	while (*s2)
+	{
+		*concanated = *s2;
+		concanated++;
+		s2++;
+	}
+	*concanated = '\0';
+	return (conc);
+}
