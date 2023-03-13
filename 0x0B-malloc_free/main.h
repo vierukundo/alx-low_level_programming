97fe13ffc8dec83e3e6022cf751c9b144cedb60e
vierukundo <vierukundo@gmail.com>
2023-03-13 05:28:38 -0700

    C - malloc, free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -6,3 +8,4 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+#endif
