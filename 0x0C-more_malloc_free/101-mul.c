b14469751bc7ed03fe404b2fd36f0e3fac54175d
vierukundo <vierukundo@gmail.com>
2023-03-17 09:05:11 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,149 +1,94 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdio.h>
+#define ERR_MSG "Error"
 /**
- * check_num - check - string there are digit
- * @str: array str
- * Return: Always 0 (Success)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int check_num(char *str)
+int is_digit(char *s)
 {
-	unsigned int count;
+	int i = 0;
 
-	count = 0;
-	while (count < strlen(str))
+	while (s[i])
 	{
-		if (!isdigit(str[count]))
-		{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		}
-		count++;
+		i++;
 	}
 	return (1);
 }
 /**
- * multiply - multiply two numbers
- * @num1: arguments
- * @num2: arguments
- * Return: array of result
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
  */
-int *multiply(char *num1, char *num2)
+int _strlen(char *s)
 {
-	static int *mul;
+	int i = 0;
 
-	unsigned int i, j, num = 0, r = 0, product = 1;
-
-	mul = malloc((strlen(num1) + strlen(num2) + 50) * sizeof(int));
-	if (strlen(num1) > strlen(num2))
+	while (s[i] != '\0')
 	{
-		for (i = 0; i < strlen(num2); i++)
-		{
-			for (j = 0; j < strlen(num1); j++)
-			{
-				if (i < strlen(num2) - 1)
-				{
-					product = (num1[j] - 48) * (num2[i] - 48) + r;
-					num = product % 10;
-					r = product / 10;
-				}
-				else
-				{
-					num = product;
-				}
-				mul[i + j] += num;
-			}
-		}
+		i++;
 	}
-	for (i = 0; i < strlen(num1); i++)
-	{
-		for (j = 0; j < strlen(num2); j++)
-		{
-			if (i < strlen(num1) - 1)
-			{
-				product = (num1[i] - 48) * (num2[j] - 48) + r;
-				num = product % 10;
-				r = product / 10;
-				mul[i + j] += num;
-			}
-			else
-			{
-				num = product;
-			}
-			mul[i + j] += num;
-		}
-	}
-	return (mul);
+	return (i);
 }
 /**
- * add_product - adds products of multiplication
- * @argv: arguments
- * Return: result
+ * errors - handles errors for main
  */
-int *add_product(char *argv[])
+void errors(void)
 {
-	int *result;
-
-	static int *mul;
-
-	unsigned int i, sum = 0, num = 0, remainder = 0;
-
-	result = multiply(argv[1], argv[2]);
-	mul = malloc((sizeof(result) + 100) * sizeof(int));
-	for (i = 0; i < sizeof(result); i++)
-	{
-		if (i < sizeof(result) - 2)
-		{
-			sum = result[i] + remainder;
-			remainder = sum / 10;
-			num = sum % 10;
-		}
-		else
-		{
-			num = sum;
-		}
-		mul[i] = num;
-	}
-	return (mul);
+	printf("Error\n");
+	exit(98);
 }
 /**
- * main - Entry point
- * @argc: Count arguments
- * @argv: Arguments
- * Return: Always 0 (Success)
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int *mul;
+	char *s1, *s2;
 
-	unsigned int i;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	int *result;
-
-	if (argc < 3 || argc > 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("%s\n", "Error");
-		exit(98);
-	}
-	result = add_product(argv);
-	mul = malloc((sizeof(result) + 50) * sizeof(int));
-	for (i = 1; i < 3; i++)
-	{
-		if (!check_num(argv[i]))
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			printf("%s\n", "Error");
-			exit(98);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < sizeof(result) / 2; i++)
+	for (i = 0; i < len - 1; i++)
 	{
-		mul[i] = result[sizeof(result) - i - 1];
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	for (i = 0; i < sizeof(mul); i++)
-	{
-		printf("%d", mul[i]);
-	}
-	printf("\n");
-	free(mul);
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
