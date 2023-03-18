c776d3697e6a511f3fcac6a2aaead22b9c6a4235
vierukundo <vierukundo@gmail.com>
2023-03-18 14:00:16 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/mull.c
+++ 0x0C-more_malloc_free/mull.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 
-	int len1, len2, len, i, carry, digit1, digit2, *result;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -77,11 +77,15 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	i = 0;
-	while (i < len - 1 && result[i] == 0)
-		i++;
-	for (; i < len - 1; i++)
-		_putchar(result[i] + '0');
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
