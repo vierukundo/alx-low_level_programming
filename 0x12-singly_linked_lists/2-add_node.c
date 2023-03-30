481c51f56c9d025325761e6511074424aa78117d
vierukundo <vierukundo@gmail.com>
2023-03-30 04:47:36 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_node - function that adds a new node
+ * at the beginning of a list_t list
+ * @head: double pointer to first node
+ * @str: element of a node
+ * Return: address of new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
