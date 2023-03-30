b865a07f339283a863b49928d7f61ee99b42d13f
vierukundo <vierukundo@gmail.com>
2023-03-30 12:28:15 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,8 +10,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	if (str == NULL)
+	unsigned int len = 0;
+
+	if (!str)
 		return (NULL);
+	while (str[len])
+		len++;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,13 +12,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	list_t *h = *head;
 
+	unsigned int len = 0;
+
 	if (!str)
 		return (NULL);
+	while (str[len])
+		len++;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	new->len = strlen(new->str);
+	new->len = len;
 	new->next = NULL;
 	if (*head == NULL)
 	{
