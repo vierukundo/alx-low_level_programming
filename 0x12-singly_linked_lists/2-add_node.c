8c3632da93f6859d48df8453dbccb1ab1722ae7a
vierukundo <vierukundo@gmail.com>
2023-03-30 05:11:09 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,8 +8,13 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new;
 
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(new->str);
 	new->next = *head;
