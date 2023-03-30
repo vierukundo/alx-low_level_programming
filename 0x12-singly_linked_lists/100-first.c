775b30e46f76b654b6dec1364b71e09d880971a3
vierukundo <vierukundo@gmail.com>
2023-03-30 10:23:30 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <unistd.h>
 /**
  * before_main - prints something before
  * main is executed
@@ -6,6 +7,8 @@
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must a"
-	"llow,\nI bore my house upon my back!\n");
+	char *s;
+
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	write(1, s, strlen(s));
 }
