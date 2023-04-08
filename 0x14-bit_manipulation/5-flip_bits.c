9fb428bc0778fbfe3fdf30791bcedd68190bf9e4
vierukundo <vierukundo@gmail.com>
2023-04-08 14:20:23 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int temp = 0, bits = 0, mask;
 
-	int i = 31;
+	int i = 63;
 
 	temp = n ^ m;
 	while (i >= 0)
 	{
-		mask = 1 << i;
-		if (temp & mask)
+		mask = temp >> i;
+		if (1 & mask)
 			bits++;
 		i--;
 	}
