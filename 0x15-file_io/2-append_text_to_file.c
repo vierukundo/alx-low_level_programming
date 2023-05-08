0461f18ccd36782deb4f0ea0a42b4293f440c8f2
vierukundo <vierukundo@gmail.com>
2023-05-08 06:11:56 -0700

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,24 +7,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		while (text_content[len])
 			len++;
 	}
-
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
 		return (-1);
-
-	close(o);
-
+	close(fd);
 	return (1);
 }
