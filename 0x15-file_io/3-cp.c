1c6da638cd228c954ab01693c3168135740513bc
vierukundo <vierukundo@gmail.com>
2023-05-08 13:37:48 -0700

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,28 +1,4 @@
 #include "main.h"
-char *create_buffer(char *file);
-void close_file(int fd);
-
-/**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
 /**
  * close_fd - a function that closes file descriptors.
  * @fd: The file descripto.
@@ -43,10 +19,6 @@ void close_fd(int fd)
  * @argc: The number of arguments
  * @argv: An array of pointers to the arguments.
  * Return: 0 on success.
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -58,7 +30,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	fd1 = open(argv[1], O_RDONLY);
 	read_bytes = read(fd1, buffer, 1024);
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
