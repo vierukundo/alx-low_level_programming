968cb1f5e0d12218ec5ec359614080f049e1aa45
vierukundo <vierukundo@gmail.com>
2023-05-08 05:29:01 -0700

    C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 	w = write(fd, text_content, len);
 	if (w == -1)
 		return (-1);
