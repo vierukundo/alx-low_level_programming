21734dbc4b9db84950ed9dff3b457d06b1c9db46
vierukundo <vierukundo@gmail.com>
2023-06-16 10:16:19 -0700

    A function that insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,44 +8,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *head = *h, *new, *temp;
-	unsigned int index = 0;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->prev = NULL;
-	new->n = n;
-	new->next = NULL;
-	if (*h == NULL && idx == 0)
+	new_node->n = n;
+	if (idx == 0)
 	{
-		*h = new;
-		return (new);
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	if (*h == NULL && idx != 0)
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+	if (temp == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (NULL);
 	}
-	for (index = 0; head != NULL; index++)
-	{
-		if (index == idx && head->prev)
-		{
-			temp = head->prev;
-			temp->next = new;
-			new->prev = temp;
-			new->next = head;
-			head->prev = new;
-			return (new);
-		}
-		else if (idx == 0 && head->prev == NULL)
-		{
-			temp = head;
-			new->next = head;
-			temp->prev = new;
-			return (new);
-		}
-		head = head->next;
-	}
-	return (NULL);
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
 }
